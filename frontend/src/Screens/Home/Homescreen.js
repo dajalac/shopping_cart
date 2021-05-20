fdc271284dd84ce15d41e34880cde76428ef6198
danielle ajala <danielleajala@gmail.com>
2021-05-20 18:51:08 -0500

    product component started

--- frontend/src/Screens/Home/Homescreen.js
+++ frontend/src/Screens/Home/Homescreen.js
@@ -3,8 +3,13 @@ import './HomeScreen.css';
 const HomeScreen = ()=>{
     return(
      <div className='home-screen'>
-         Home screen
+         <h2 className='home-screen-title'>Latest products</h2>
+
+         <div className='home-screen-products'>
+             <Product/>
+         </div>
      </div>
+
     )
 };
 
---
+++ frontend/src/components/Products/Products.js
@@ -0,0 +1,18 @@
+import './Products.css';
+
+const Products = ()=>{
+    return(
+     <div className='products'>
+        <img src ='' alt=''/>
+
+        <div className='product-info'>
+            <p className='product-name'></p>
+            <p className='product-description' ></p>
+
+        </div>
+     </div>
+
+    )
+};
+
+export default Products;
