315849052d2e7a8e3971e757983e37ad8e435cb3
danielle ajala <danielleajala@gmail.com>
2021-05-26 20:26:45 -0500

    cartIntem implemented with redux

--- frontend/src/Screens/Cart/CartScreen.js
+++ frontend/src/Screens/Cart/CartScreen.js
@@ -1,23 +1,64 @@
 import'./CartScreen.css';
 import CartItem from '../../components/CartItem/CartItem'
 
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+
+
+// Actions
+import { addToCart, removeFromCart } from '../../redux/actions/cartActions';
+
 const CartScreen = ()=>{
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+ 
+
+  const qtyChangeHandler = (id, qty) => {
+    dispatch(addToCart(id, qty));
+  };
+
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
+  };
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  };
+
+  const getCartSubTotal = () => {
+    return cartItems
+      .reduce((price, item) => price + item.price * item.qty, 0)
+      .toFixed(2);
+  };
+
     return(
      <div className='cart-screen'>
          <div className='cart-screen-left'>
              <h2>Shopping Cart</h2>
-
-             <CartItem/>
-             <CartItem/>
-             <CartItem/>
-             <CartItem/>
-             <CartItem/>
+             {cartItems.length === 0 ? (
+            <div>
+              Your Cart Is Empty <Link to="/">Go Back</Link>
+            </div>
+          ) : (
+            cartItems.map((item) => (
+              <CartItem
+                key={item.product}
+                item={item}
+                qtyChangeHandler={qtyChangeHandler}
+                removeHandler={removeFromCartHandler}
+              />
+            ))
+          )}
+             
 
          </div>
          <div className='cart-screen-right'>
              <div className='cart-screen-info'>
-                <p>Subtotal (0) items</p>
-                <p>$1.99</p>
+             <p>Subtotal ({getCartCount()}) items</p>
+            <p>${getCartSubTotal()}</p>
              </div>
              <div>
                  <button>Check out</button>
--- frontend/src/components/CartItem/CartItem.js
+++ frontend/src/components/CartItem/CartItem.js
@@ -1,26 +1,31 @@
 import './CartItem.css';
 import { Link } from 'react-router-dom';
 
-const CartItem = () => {
+const CartItem = ({ item, qtyChangeHandler, removeHandler }) => {
     return (
         <div className='cart-item'>
             <div className='cart-item-image'>
-                <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
-                    alt='product name' />
+                <img src={item.imageUrl}
+                    alt={item.name} />
             </div>
-            <Link to={`/product/${111}`} className='cart-item-name'>
-                <p> product name</p>
+            <Link to={`/product/${item.product}`} className='cart-item-name'>
+                <p> {item.name}</p>
             </Link>
-            <p className='cart-item-price'>$1.99</p>
+            <p className='cart-item-price'>${item.price}</p>
 
-            <select className='cart-item-select'>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
+            <select
+                value={item.qty}
+                onChange={(e) => qtyChangeHandler(item.product, e.target.value)}
+                className="cartItem__select"
+            >
+                {[...Array(item.countInStock).keys()].map((x) => (
+                    <option key={x + 1} value={x + 1}>
+                        {x + 1}
+                    </option>
+                ))}
             </select>
 
-            <button className='cart-item-deletebtn'>
+            <button className='cart-item-deletebtn' onClick={() => removeHandler(item.product)}>
                 <i className='fas fa-trash'></i>
             </button>
         </div>
